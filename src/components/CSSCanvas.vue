ebd14b5a0fbbb8ff4350aee6c167ca3c44c908fa
Giga <gigabyte5671@gmail.com>
2023-02-02 04:45:24 +1300

    Create the output canvas component

---
+++ src/components/CSSCanvas.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'CSSCanvas',
+});
+</script>
+
+<template>
+	<output
+		class="cssCanvas"
+	>
+	</output>
+</template>
+
+<style scoped>
+.cssCanvas {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	background-color: var(--color-background);
+	transition: 0.2s ease background-color;
+}
+</style>
