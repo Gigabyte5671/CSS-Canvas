87607accf75b5286a0ea3b6686d6e4bcde215981
Giga <gigabyte5671@gmail.com>
2023-03-08 14:15:43 +1300

    Use single-quotes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  envPrefix: ["VITE_"],
+  envPrefix: ['VITE_'],
   build: {
-    target: ["es2021", "chrome100", "safari13"],
+    target: ['es2021', 'chrome100', 'safari13'],
     // don't minify for debug builds
-    minify: "esbuild",
+    minify: 'esbuild',
     // produce sourcemaps for debug builds
-    sourcemap: process.env.NODE_ENV === "production" ? false : true
+    sourcemap: process.env.NODE_ENV === 'production' ? false : true
   },
-  base: "/"
+  base: '/'
 });
